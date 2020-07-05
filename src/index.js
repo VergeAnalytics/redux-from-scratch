a1d644e5e6e7889a085f58bd912de78cd4a418d9
PuneetSivananda <psivananda@chatsoft.com>
2020-07-05 20:55:57 +0530

    feat: building blocks of redux

--- src/index.js
+++ src/index.js
@@ -1,36 +1,19 @@
-const person = {name:"John",
-          address:
-          {
-            city:"SF",country:"USA"}
-          }
-// const updated = Object.assign({},person,{name:"Bob", age:"30"})
-// const updated = {...person,name:"Bob", age:"30"} //spread operator shallow copy
-const updated = {...person,
-  address:{
-    ...person.address,
-    city:"SF"
+import {store}  from "./store"
 
-  },
-  name:"Bob", age:"30"} //spread operator deep copy
-// updated.address.city = "NY"
-console.log(updated)
-// console.log(person)
+const unsubscribe = store.subscribe(()=>{
+  console.log("Store Changed", store.getState())
+})
 
+store.dispatch({
+  type:"bugAdded",
+  payload:  {description:"Bug 1 Added"}
+})
 
+unsubscribe()
 
-const numbers =[1,2,3]
-const added = [4,...numbers]
-console.log(added)
-const index = numbers.indexOf(2)
-const added1  = [
-  ...numbers.slice(0,index),
-  4,
-  ...numbers.slice(index)
-]
-console.log(added1)
+store.dispatch({
+  type:"bugRemoved",
+  payload:  {id:1}
+})
 
-//filter for removing
-console.log(numbers.filter(n => n !==2))
-//updating map
-console.log(numbers.map(n => n==2 ?20:n))
-
+console.log(store.getState())
